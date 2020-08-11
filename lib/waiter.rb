5bf683e8b621460ff3715915edbf651fa5294d8a
Eugene O'Neil <ejo7220@gmail.com>
2020-08-11 19:28:57 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -3,7 +3,35 @@ attr_accessor :name, :yrs_experience
 
 @@all = []
 
-
+  def initialize (name, yrs_experience)
+  
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(customer, total, tip=0)
+    meal.new(self, customer, total, tip)
+  end
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  end
+  
+  
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    best_tipped_meal.customer
+  end
+  
   
 end
 
